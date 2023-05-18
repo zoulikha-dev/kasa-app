11e40e123801ccc4641f7000faa89f1001fac358
zoulikha <zaz13@me.com>
2023-05-18 05:00:28 +0200

    add css container thumb

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Header from "../../components/Header/Header";
 
 const About = () => {
   return (
     <div>
-        <h1>About</h1>
+      <h1>About</h1>
     </div>
-  )
+  );
 };
 
 export default About;
