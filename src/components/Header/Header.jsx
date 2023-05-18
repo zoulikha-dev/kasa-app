edd2abcdb56501ff731b9db4964d949dd0ed7eeb
zoulikha <zaz13@me.com>
2023-05-18 02:24:06 +0200

    Creation header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <header className="header">azert</header>;
+  return (
+    <header>
+      <img className="logo" src="/img/logo.png" alt="Logo Kasa" />
+      <nav>
+        <div className="nav">
+          <ul className="menu">
+            <li>
+              <Link to="/" className="lien">
+                Accueil
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="lien">
+                A Propos
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
