3a8e59e37dc68ca51ba5ad08f1a3f4b1628f2227
zoulikha <zaz13@me.com>
2023-05-18 02:57:04 +0200

    creation footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <div className="logo-container">
+        <img className="logo2" src="/img/logo-2.png" alt="Logo Kasa" />
+        <img
+          className="logo-footer"
+          src="/img/logo-footer.png"
+          alt="Tous droits réservés"
+        />
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
