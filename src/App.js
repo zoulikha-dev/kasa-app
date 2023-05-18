8e4aaf1605d0027b6b2db284c78798cbcdd9601a
zoulikha <zaz13@me.com>
2023-05-18 02:26:11 +0200

    Initialisation app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import About from "./pages/About/About";
+import Error404 from "./pages/Error404/Error404";
+import Home from "./pages/Home/Home";
+import Housing from "./pages/Housing/Housing";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/erreur404" element={<Error404 />} />
+          <Route path="/logements/:id" element={<Housing />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
