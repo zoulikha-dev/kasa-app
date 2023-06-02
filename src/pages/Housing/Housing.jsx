9038f89bd66a77f88b7f54eea18bbd539349b634
zoulikha <zaz13@me.com>
2023-06-02 23:47:41 +0200

    creation housing.jsx

--- src/pages/Housing/Housing.jsx
+++ src/pages/Housing/Housing.jsx
@@ -1,15 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react"; // Importation des composants React nécessaires
 import Tag from "../../components/Tag/Tag";
+import Collapse from "../../components/Collapse/Collapse";
 import "./Housing.css";
+import Carousel from "../../components/Carousel/Carousel";
+import { useParams } from "react-router-dom";
+import Host from "../../components/Host/Host";
 
-const Logements = () => {
+const Housing = () => {
+  const { id } = useParams(); // Récupération de la valeur de l'ID à partir des paramètres d'URL grâce au hook useParams
+  const [housing, setHousing] = useState(); // Initialisation du state housing avec une valeur par défaut vide
+
+  //UseEffect permet de déclencher le Fetch et useState permet de stocker le retour de l'pi dans le state
+  useEffect(() => {
+    fetch("/data.json")
+      .then((response) => response.json()) //Conversion de la réponse en JSON
+      .then((jsonData) => {
+        const foundHousing = jsonData.find((house) => house.id == id); // Recherche du logement correspondant à l'ID
+        console.log(foundHousing); // Affichage du logement trouvé dans la console
+        //Cette condition vérifie si le logement à été trouvé. dans ce cas setPictures est utilisé pour mettre à jour le stat pictures avec la liste d'image trouvé. (...)permet d'afficher les img
+        if (foundHousing) {
+          setHousing(foundHousing); // Mise à jour du state housing avec le logement trouvé
+        }
+      })
+      .catch((error) => console.log(error)); // Gestion des erreurs
+  }, []);
+
   return (
-    <div className="container-tag">
-      <Tag text="Cosy" />
-      <Tag text="Canal" />
-      <Tag text="Paris 10" />
-    </div>
+    <>
+      {housing ? (
+        <div>
+          <div className="container-carroussel">
+            <Carousel images={housing.pictures} />
+          </div>
+
+          <div>
+            <div>
+              <h1 className="housing-title"> {housing.title} </h1>
+              <p className="housing-location"> {housing.location} </p>
+              <div className="container-tag">
+                {housing.tags.map((tag, i) => (
+                  <Tag text={tag} key={tag + i} />
+                ))}
+              </div>
+            </div>
+            <div className="rating">
+              <Host name={housing.host.name} picture={housing.host.picture} />
+            </div>
+          </div>
+
+          <div className="container-collapse">
+            <Collapse label="Description">
+              <div className="container-description">
+                <p className="content-description">{housing.description}</p>{" "}
+                //Paragraphe avec le contenu de la description du logement
+              </div>
+            </Collapse>
+            <Collapse label="Équipements">
+              <div className="container-liste">
+                <ul className="content-liste">
+                  {housing.equipments && // Vérification que la liste des équipements existe dans le logement
+                    housing.equipments.map((equipments, index) => (
+                      <li key={index}>{equipments}</li> // Élément de liste pour chaque équipement
+                    ))}
+                </ul>
+              </div>
+            </Collapse>
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 };
 
-export default Logements;
+export default Housing;
