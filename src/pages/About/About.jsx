f3b468bd417dc400cd24488903df3b155a824bac
zoulikha <zaz13@me.com>
2023-06-02 23:46:27 +0200

    creation about.jsx

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,32 @@
 import React from "react";
-import Header from "../../components/Header/Header";
+import Collapse from "../../components/Collapse/Collapse";
+import Banner from "../../components/Banner/Banner";
+import "./About.css";
 
 const About = () => {
+  const bannerImage = "/banner2.png";
+
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <>
+      <div className="container-banner2">
+        <Banner image={bannerImage} />
+      </div>
+
+      <div className="collapse">
+        <Collapse label="Fiabilité" />
+        <Collapse label="Respect">
+          <div className="content-p">
+            <p className="content-collapse ">
+              La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+              comportement <br /> discriminatoire ou de perturbation du
+              voisinage entraînera une exclusion de <br /> notre plateforme.
+            </p>
+          </div>
+        </Collapse>
+        <Collapse label="Service" />
+        <Collapse label="Sécurité" />
+      </div>
+    </>
   );
 };
 
