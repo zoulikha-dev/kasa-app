1bb08636196a2d19790daf73f9e5743d3cb70815
zoulikha <zaz13@me.com>
2023-06-02 23:35:21 +0200

    creation composant

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Error404.css";
 
 const Erreur404 = () => {
   return (
     <div>
       <h1>404</h1>
+      <p>Oups! la page que vous demandez n'existe pas.</p>
+      <div className="lien-accueil">
+        <Link to="/" className="lien">
+          Retourner sur la page d'accueil
+        </Link>
+      </div>
     </div>
   );
 };
