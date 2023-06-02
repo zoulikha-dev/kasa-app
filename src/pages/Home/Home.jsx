0fa84ee9f529c5c303f6f37e26eb4812036f5e04
zoulikha <zaz13@me.com>
2023-06-02 23:47:02 +0200

    modif home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,38 @@
 import React, { useEffect, useState } from "react"; //composant react
+import Banner from "../../components/Banner/Banner";
 import Thumb from "../../components/Thumb/Thumb";
 import "./Home.css";
 
 const Home = () => {
-  //UseEffect permet de déclencher le Fetch et useState permet de stocker le retour de l'pi dans le state
-  const [data, setData] = useState([]); // Déclaration de l'état "data" initialisé avec un tableau vide (data est l'état qui contiendra les données)
+  //UseEffect permet de déclencher le Fetch et useState permet de stocker le retour de l'api dans le state
+  const [housings, setHousings] = useState([]); // Déclaration de l'état "housing" initialisé avec un tableau vide
 
   useEffect(() => {
     fetch("/data.json")
       .then((response) => response.json()) //Conversion de la réponse en JSON
       .then((jsonData) => {
-        setData(jsonData); // Mise à jour de l'état data avec les données JSON récupérées
+        setHousings(jsonData); // Mise à jour de l'état data avec les données JSON récupérées
         console.log(jsonData); // Affiche les données JSON dans la console
       })
       .catch((error) => console.log(error));
   }, []);
 
+  const bannerImage = "/banner1.png";
+
   return (
     <main>
+      <div className="container-banner">
+        <Banner image={bannerImage} titre={`Chez vous, partout et ailleurs`} />
+      </div>
       <div className="container-thumb">
-        {/* c'est une boucle de transformation qui parcourt chaque élément du tableau data et éxécute la fonction de rendu spécifiée entre parenthèses */}
-        {data.map((item) => (
-          <Thumb key={item.id} title={item.title} image={item.cover} />
+        {housings.map((housing) => (
+          <Thumb
+            key={housing.id}
+            title={housing.title}
+            cover={housing.cover}
+            id={housing.id}
+          />
         ))}
-        {/* cette ligne de code crée un composant Thumb pour chaque élément dans le tableau data en lui passant les props nécessaires pour afficher le titre et l'image de chaque élément. */}
       </div>
     </main>
   );
