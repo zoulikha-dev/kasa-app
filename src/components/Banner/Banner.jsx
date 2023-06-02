9c8634e929035288bbcbc6858a1f7b4657569ac2
zoulikha <zaz13@me.com>
2023-06-02 23:42:13 +0200

    add title banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Banner.css";
-const Banner = ({ image }) => {
+const Banner = ({ image, titre }) => {
   return (
     <div className="banner">
       <img src={image} alt="Banner" />
+      <h1> {titre} </h1>
     </div>
   );
 };
