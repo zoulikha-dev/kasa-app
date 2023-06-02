fd4e9450cd6af6d2f6e8afbb79b7ebad86a5a898
zoulikha <zaz13@me.com>
2023-06-02 23:45:16 +0200

    modif tag

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./Tag.css";
 
 const Tag = ({ text }) => {
-  return <div className="tag"> {text} </div>;
+  return (
+    <div className="tag">
+      <p>{text}</p>
+    </div>
+  );
 };
 
 export default Tag;
