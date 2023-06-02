5552218a20fcf2ef97457ddbdfb050f58810c4c7
zoulikha <zaz13@me.com>
2023-06-02 23:43:16 +0200

    creation collapse

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Collapse.css";
 
-const Collapse = () => {
-  return <div></div>;
+const Collapse = (props) => {
+  const [isOpen, setIsOpen] = useState(false); //isOpen est initialisé à false, ce qui veux dire que le contenu du collapse est masqué
+  return (
+    <div className="collapse">
+      <button className="toggle" onClick={() => setIsOpen(!isOpen)}>
+        {/* A chaquefois que l'on clique sur le bouton, la fonction fléchée est
+        exécutée. //setIsopen ... inverse la valeur de isOpen. */}
+        {props.label}
+      </button>
+      {isOpen && <div className="content"> {props.children} </div>}
+      {/* si isOpen est true alors le contenu entre les balises d'ouverture et de
+      fermeture est affiché */}
+    </div>
+  );
 };
 
 export default Collapse;
