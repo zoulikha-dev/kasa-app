e1d48033d106db4e0236e3755c0df8946abd73ce
zoulikha <zaz13@me.com>
2023-06-02 23:42:55 +0200

    creation composant carroussel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+// import "./Caroussel.css";
 
-const Carousel = () => {
-  return <div></div>;
+const Carousel = ({ images }) => {
+  //Déclare une variable d'état "currentSLide" pour suivre l'index de l'image actuelle ds le diapo
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentSlide((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+    // Si l'index précédent (prevIndex) est égal à 0, on passe à la dernière image
+    // Sinon, on retire l'index pour passer à la diapositive précédente
+  };
+
+  const nextSlide = () => {
+    setCurrentSlide((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+    // Si l'index précédent (prevIndex) est égal à l'index de la dernière image,
+    // on passe à la première image (index 0)
+    // Sinon, on incrémente l'index pour passer à la diapositive suivante
+  };
+  return (
+    <div>
+      <button onClick={prevSlide}>Prev</button>
+      <img src={images[currentSlide]} alt="Slide" />
+      <button onClick={nextSlide}>Next</button>
+    </div>
+  );
 };
 
 export default Carousel;
