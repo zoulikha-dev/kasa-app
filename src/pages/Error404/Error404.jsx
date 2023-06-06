c5fc58105ba2dcc622f837c7518c42f59e4ba312
zoulikha <zaz13@me.com>
2023-06-06 10:05:52 +0200

    modif error.jsx

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -5,7 +5,7 @@ import "./Error404.css";
 const Erreur404 = () => {
   return (
     <div>
-      <h1>404</h1>
+      <h3>404</h3>
       <p>Oups! la page que vous demandez n'existe pas.</p>
       <div className="lien-accueil">
         <Link to="/" className="lien">
