1ba6bb473dd04ae324df2c52552b7e8cd5f714b5
zoulikha <zaz13@me.com>
2023-06-06 10:47:56 +0200

    add paragraphe about.jsx

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -13,7 +13,16 @@ const About = () => {
       </div>
       <main className="collapse1">
         <div className="collapse">
-          <Collapse label="Fiabilité" />
+          <Collapse label="Fiabilité">
+            <div className="content-p">
+              <p className="content-collapse ">
+                Les annonces postées sur Kasa garantissent une fiabilité totale.
+                Les photos sont conformes aux logements, et toutes les
+                informations sont régulièrement vérifiées par nos équipes.
+              </p>
+            </div>
+          </Collapse>
+
           <Collapse label="Respect">
             <div className="content-p">
               <p className="content-collapse ">
@@ -23,8 +32,30 @@ const About = () => {
               </p>
             </div>
           </Collapse>
-          <Collapse label="Service" />
-          <Collapse label="Sécurité" />
+
+          <Collapse label="Service">
+            <div className="content-p">
+              <p className="content-collapse ">
+                Nos équipes se tiennent à votre disposition pour vous fournir
+                une expérience parfaite. N'hésitez pas à nous contacter si vous
+                avez la moindre question.
+              </p>
+            </div>
+          </Collapse>
+
+          <Collapse label="Sécurité">
+            <div className="content-p">
+              <p className="content-collapse ">
+                La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes
+                que pour les voyageurs, chaque logement correspond aux critères
+                de sécurité établis par nos services. En laissant une note aussi
+                bien à l'hôte qu'au locataire, cela permet à nos équipes de
+                vérifier que les standards sont bien respectés. Nous organisons
+                également des ateliers sur la sécurité domestique pour nos
+                hôtes.
+              </p>
+            </div>
+          </Collapse>
         </div>
       </main>
     </>
