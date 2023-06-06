36dfddf7b35767576da932d43d9072d1e5120f1a
zoulikha <zaz13@me.com>
2023-06-06 10:01:15 +0200

    creation composant rating

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Rating = (rating) => {
-  return <div></div>;
+const Rating = ({ rating }) => {
+  const stars = []; //Tableau pour stocker les étoiles
+
+  // Boucle for pour générer les étoiles en fonction de la valeur de rating
+  for (let index = 0; index < rating; index++) {
+    stars.push(<img key={index + rating} src="/pink-star.png" alt="Etoile" />);
+  } // Ajoute une balise <img> à l'array stars pour chaque étoile
+
+  return <div>{stars}</div>;
 };
 
 export default Rating;
