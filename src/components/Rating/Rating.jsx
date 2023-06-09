8a2518196c2f69ac18c8b93a35fa65fbc3f6a008
zoulikha <zaz13@me.com>
2023-06-09 14:17:19 +0200

    add grey star

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -4,8 +4,14 @@ const Rating = ({ rating }) => {
   const stars = []; //Tableau pour stocker les étoiles
 
   // Boucle for pour générer les étoiles en fonction de la valeur de rating
-  for (let index = 0; index < rating; index++) {
-    stars.push(<img key={index + rating} src="/pink-star.png" alt="Etoile" />);
+  for (let index = 0; index < 5; index++) {
+    stars.push(
+      <img
+        key={index + rating}
+        src={index < rating ? "/pink-star.png" : "/grey-star.png"}
+        alt="Etoile"
+      />
+    );
   } // Ajoute une balise <img> à l'array stars pour chaque étoile
 
   return <div>{stars}</div>;
