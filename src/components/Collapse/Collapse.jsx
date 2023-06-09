32973a8d9b203d3a725d4bbb2df76288f04bf8b8
zoulikha <zaz13@me.com>
2023-06-09 14:16:36 +0200

    correction collapse.jsx

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import "./Collapse.css";
 
-const Collapse = (props) => {
+const Collapse = ({ label, children }) => {
   const [isOpen, setIsOpen] = useState(false); //isOpen est initialisé à false, ce qui veux dire que le contenu du collapse est masqué
 
   return (
-    <div>
+    <div className="collapse-block">
       <button className="toggle" onClick={() => setIsOpen(!isOpen)}>
         {/* A chaquefois que l'on clique sur le bouton, la fonction fléchée est
         exécutée. //setIsopen ... inverse la valeur de isOpen. */}
-        {props.label}
+        <span>{label}</span>
+        <img src={isOpen ? "/up-arrow.png" : "/down-arrow.png"} alt="flèche" />
       </button>
-      {isOpen && <div className="content"> {props.children} </div>}
+      {isOpen && <div className="content"> {children} </div>}
       {/* si isOpen est true alors le contenu entre les balises d'ouverture et de
       fermeture est affiché */}
     </div>
